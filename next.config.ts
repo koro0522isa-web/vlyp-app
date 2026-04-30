2434311456df32942e5f41c26798488807301942
koro0522isa <koro0522isa@gmail.com>
2026-05-01 04:49:50 +0900

    fix: explicitly set security headers to unsafe-none

--- next.config.ts
+++ next.config.ts
@@ -18,7 +18,21 @@ const nextConfig: NextConfig = {
     ],
   },
   async headers() {
-    return [];
+    return [
+      {
+        source: '/(.*)',
+        headers: [
+          {
+            key: 'Cross-Origin-Opener-Policy',
+            value: 'unsafe-none',
+          },
+          {
+            key: 'Cross-Origin-Embedder-Policy',
+            value: 'unsafe-none',
+          },
+        ],
+      },
+    ];
   },
 };
 
